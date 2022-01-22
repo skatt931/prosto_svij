f03c4066d772d38101a0d2af72be0a756aaf09cb
skatt931 <ihor.kurnytskyi@sinnerschrader.com>
2022-01-22 20:45:21 +0100

    feat: add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -173,6 +173,20 @@ module.exports = {
         icon: `src/images/icon.png`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+         // You can add multiple tracking ids and a pageview event will be fired for all of them.
+         trackingIds: [
+          "G-EGDH0H2YZM", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
   ],
 };
